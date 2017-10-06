9726a69c89ffb826da5ce1f63c649f00812dde91
namunam <maldonadoruizmanuel@gmail.com>
2017-10-06 21:06:17 +0200

    All

---
+++ ProyectoSoftwareApp/HelloWorldAPP/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldAPP
+{
+    class Program
+    {
+        private static List<string> l;
+        private static void InitList()
+        {
+            l = new List<string>()
+            {
+                "Australian ",
+                "Mongolian ",
+                "Russian ",
+                "Austrian ",
+                "Brazillean "
+            };
+        }
+        static void Main(string[] args)
+        {
+            InitList();
+            l.Sort();
+            foreach(string s in l)
+            {
+                Console.Write(s);
+            }
+            Console.ReadKey();
+        }
+    }
+}
